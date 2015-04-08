b231043f589a32d13974ca702c8115c1cce1b06a
Mateusz Czapliński <czapkofan@gmail.com>
2015-04-08 23:30:48 +0200

    php2go: more conversions

--- php2go.go
+++ php2go.go
@@ -48,6 +48,9 @@ func main() {
 		line.ReplaceAll(`^SVG;$`, "`")
 		line.ReplaceAll(`\.=`, `+=`)
 		line.ReplaceAll(`\bnew\s+`, `New`)
+		line.ReplaceAll(`\bnull\b`, `nil`)
+		line.ReplaceAll(`\bself::`, class+`::`)
+		// line.ReplaceAll(`\b__construct\b`, `New`+class)
 		for before, after := range replacements {
 			line.ReplaceAll(before, after)
 		}
@@ -57,16 +60,50 @@ func main() {
 		// class X -> type X struct
 		case line.Find(`^class\s+(\w+)`):
 			class = line.M[1]
-			replacements["self::"] = class + "_"
+			// replacements["self::"] = class + "_"
 			fmt.Printf("type %s struct%s\n",
 				class, line.Text[len(line.M[0]):])
 
-		case line.Find(`^(\s*)public\s+static\s+function\s+(\w+)`):
-			oldname := fmt.Sprintf("%s::%s", class, line.M[2])
-			newname := fmt.Sprintf("%s_%s", class, publicize(line.M[2]))
+		case line.Find(`^}\s*$`):
+			class = ""
+
+		// static fields/methods
+		case line.Find(`^(\s*)(public|private)\s+static\s+(function\s+)?(\w+)`):
+			oldname := fmt.Sprintf(`\b%s::%s\b`, class, line.M[4])
+			if line.M[2] == "public" {
+				line.M[4] = publicize(line.M[4])
+			}
+			newname := fmt.Sprintf("%s_%s", class, line.M[4])
 			replacements[oldname] = newname
-			fmt.Printf("%sfunc %s%s\n",
-				line.M[1], newname, line.Text[len(line.M[0]):])
+			kind := "var"
+			if strings.HasPrefix(line.M[3], "function") {
+				kind = "func"
+			}
+			fmt.Printf("%s%s %s%s\n",
+				line.M[1], kind, newname, line.Text[len(line.M[0]):])
+
+		case line.Find(`^(\s*)(public|private)\s+(function\s+)?(\w+)`):
+			// TODO(akavel): find .name instead of name?
+			oldname := fmt.Sprintf(`\b%s\b`, line.M[4])
+			if line.M[2] == "public" {
+				line.M[4] = publicize(line.M[4])
+			}
+			newname := line.M[4]
+			replacements[oldname] = newname
+			kind := ""
+			if strings.HasPrefix(line.M[3], "function") {
+				kind = fmt.Sprintf("func (this *%s) ", class)
+			}
+			fmt.Printf("%s%s%s%s\n",
+				line.M[1], kind, newname, line.Text[len(line.M[0]):])
+
+		case line.Find(`^(\s*)foreach\s*\(\s*(.+?)\s+as\s+(\w+)\s*\)`):
+			array := line.M[2]
+			variable := line.M[3]
+			fmt.Printf("%sfor _, %s := range %s%s\n",
+				line.M[1], variable, array, line.Text[len(line.M[0]):])
+
+		// case line.Find(`^(\s*)public\s+static\s+
 
 		// print rest of lines intact
 		default:
